1cd4b569df51147d43088c001a0a35594dc0d7cb
Ömer <omergurbuz25@hotmail.com>
2023-07-21 17:06:08 +0300

    seeds

--- data/seeds/users-0.js
+++ data/seeds/users-0.js
@@ -1,13 +1,49 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+
+const defaultUsers = [
+  {
+    username: "John Doe",
+    email: "johndoe@example.com",
+    password: "$2a$08$394J9.utNucp1CayyYxtdej8Ck27wDV9BTsUrhRMfJsfoFyqyUh8a",
+    role: "admin",
+  },
+  {
+    username: "Jane Doe",
+    email: "janedoe@example.com",
+    password: "$2a$08$394J9.utNucp1CayyYxtdej8Ck27wDV9BTsUrhRMfJsfoFyqyUh8a",
+    role: "user",
+  },
+  {
+    username: "Peter Smith",
+    email: "petersmith@example.com",
+    password: "$2a$08$394J9.utNucp1CayyYxtdej8Ck27wDV9BTsUrhRMfJsfoFyqyUh8a",
+    role: "user",
+  },
+  {
+    username: "Mary Jones",
+    email: "maryjones@example.com",
+    password: "$2a$08$394J9.utNucp1CayyYxtdej8Ck27wDV9BTsUrhRMfJsfoFyqyUh8a",
+    role: "user",
+  },
+  {
+    username: "David Brown",
+    email: "davidbrown@example.com",
+    password: "$2a$08$394J9.utNucp1CayyYxtdej8Ck27wDV9BTsUrhRMfJsfoFyqyUh8a",
+    role: "user",
+  },
+];
+
+exports.users = defaultUsers;
+
+exports.seed = async function (knex) {
+  await knex("users").truncate();
+  await knex("users").insert(defaultUsers);
+};
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
-  ]);
+  await knex("table_name").del();
+  await knex("table_name").insert();
 };
